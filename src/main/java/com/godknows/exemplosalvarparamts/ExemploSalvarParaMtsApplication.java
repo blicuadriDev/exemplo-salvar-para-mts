1b118ccf4c9f92d857f3f297ea6670e227c87e2b
blicuadriDev <blicuadriDev@gmail.com>
2023-08-01 16:02:53 -0300

    Remove tomcat´s dependancy from poms.xml for shutting application down

--- src/main/java/com/godknows/exemplosalvarparamts/ExemploSalvarParaMtsApplication.java
+++ src/main/java/com/godknows/exemplosalvarparamts/ExemploSalvarParaMtsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExemploSalvarParaMtsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ExemploSalvarParaMtsApplication.class, args);
 	}
 
